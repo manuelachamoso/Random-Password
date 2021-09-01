90e7c23f86bae0d33a39cfb5105f5623ba2fd1b4
Manuela Chamoso <manuela.chamoso@gmail.com>
2021-09-01 12:07:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!"$%&/()=?@`.;:+=¨^*_-0123456789';
+
+let output = document.getElementById('output');
+
+
+function randomValue(value) {
+    return Math.floor(Math.random()*value);
+}
+
+function genPassword() {
+    let length = document.getElementById('length').value;
+    document.getElementById('length-val').textContent = length;
+    let str = '';
+
+    for (let i = 0; i < length; i++) {
+        let random = randomValue(characters.length);
+        str += characters.charAt(random);
+    }
+    output.value = str;
+}
+
+function copy() {
+    output.select();
+    document.execCommand('copy');
+    alert('Password copied!');
+}
+genPassword();
